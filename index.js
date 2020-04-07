44e43cee0deec9c187b20edfbb49f05be8204b4b
Arushi Jain <arushijain1479@gmail.com>
2020-04-07 21:26:21 +0530

    feat:Added basic bot created

---
+++ index.js
@@ -0,0 +1,14 @@
+const api = require('./api');
+
+api.on('message', async (message) => {
+    console.log(message);
+    try{
+    if (message.text === 'hi') {
+        api.sendMessage({
+            chat_id: message.chat.id,
+            text: 'You are going to die',
+        });
+    }
+}catch(err)
+{console.log(err);}
+});
