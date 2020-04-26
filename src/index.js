54649b4b6fbf450f897bf972353aa1249434fb44
Arushi Jain <arushijain1479@gmail.com>
2020-04-26 13:02:14 +0530

    someMessage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const api = require('./api');
 require('./models/db');
 // const Store = require('./models/store.model');
 const getData = require('./getData');
+const strings = require('./strings');
 
 let j = 0;
 
@@ -12,53 +13,80 @@ api.on('message', async (message) => {
     const doc = await getData.store();
     const info = (JSON.parse(doc.data)).statewise;
     try {
-        if (j === 0) {
+        if (!message.reply_to_message) {
             if (/^hi$/i.test(message.text)) {
                 api.sendMessage({
                     chat_id: message.chat.id,
-                    text: 'You are going to die',
+                    text: strings.onHi,
                 });
-            }
-
-            if (message.text === '/start') {
+            } else if (message.text === strings.askStart) {
                 api.sendMessage({
                     chat_id: message.chat.id,
-                    text:
-
-               `Welcome human.
-               
-Just ask me the count of the confirmed cases by sending me 
-/count - For the total count 
-/state (followed by state name) - For the statewise count
-
-P.S. Don't say hi to me`,
+                    text: strings.onStart,
                 });
-            }
-            if (message.text === '/count') {
+            } else if (message.text === strings.askAbout) {
                 api.sendMessage({
                     chat_id: message.chat.id,
-                    text: info[0].confirmed,
+                    text: strings.onAbout,
                 });
-            }
-            if (message.text === '/state') {
-                j = 1;
+            } else if (message.text === strings.askCount) {
                 api.sendMessage({
                     chat_id: message.chat.id,
-                    text: 'Enter state:',
+                    text: strings.onCount(info[0].confirmed, info[0].active, info[0].recovered,
+                        info[0].deaths, info[0].lastupdatedtime.slice(0, 16)),
+                });
+            } else if (message.text === strings.askStatewise) {
+                let i;
+                for (i = 1; i < (info).length; i += 1) {
+                    api.sendMessage({
+                        chat_id: message.chat.id,
+                        text: strings.onStateName(info[i].state, info[i].confirmed,
+                            info[i].active, info[i].recovered,
+                            info[i].deaths, info[i].lastupdatedtime.slice(0, 16)),
+                    });
+                }
+            } else if (message.text === strings.askState) {
+                api.sendMessage({
+                    chat_id: message.chat.id,
+                    text: strings.onState,
+                    reply_markup: JSON.stringify({
+                        force_reply: true,
+                    }),
+                });
+            } else {
+                api.sendMessage({
+                    chat_id: message.chat.id,
+                    text: strings.tryAgainMessage,
                 });
             }
-        } else {
-            j = 0;
+        } else
+        if (message.reply_to_message.text === strings.onState) {
             let i;
             for (i = 0; i < (info).length; i += 1) {
                 const a = info[i].state;
                 if (message.text.toLowerCase() === a.toLowerCase()) {
+                    j = 1;
                     api.sendMessage({
                         chat_id: message.chat.id,
-                        text: info[i].confirmed,
+                        text: strings.onStateName(info[i].state, info[i].confirmed,
+                            info[i].active, info[i].recovered,
+                            info[i].deaths, info[i].lastupdatedtime.slice(0, 16)),
                     });
                 }
             }
+            if (j === 0) {
+                api.sendMessage({
+                    chat_id: message.chat.id,
+                    text: strings.tryAgainMessage,
+                });
+            } else {
+                j = 0;
+            }
+        } else {
+            api.sendMessage({
+                chat_id: message.chat.id,
+                text: strings.tryAgainMessage,
+            });
         }
     } catch (err) { console.log(err); }
 });
